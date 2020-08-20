6a92c44ef2efcd7951c6aa0a2eb6611efa038e44
jbaumann2 <jan.baumann2@outlook.com>
2020-08-20 22:38:32 +0200

    Added persistence features to asset allocation form #11

--- Entities/AssetAllocationEntry.cs
+++ Entities/AssetAllocationEntry.cs
@@ -1,11 +1,55 @@
-﻿namespace Watchdog.Entities
+﻿using System.Collections.Generic;
+using Watchdog.Persistence;
+
+namespace Watchdog.Entities
 {
-    public class AssetAllocationEntry
+    public class AssetAllocationEntry : Persistable
     {
-        private AssetClass AssetClass { get; set; }
-        private Currency Currency { get; set; }
-        private double Value { get; set; }
+        private string tableName = "wdt_asset_allocation_entries";
+        private Dictionary<string, string> tableMapping = new Dictionary<string, string>
+        {
+            {"asset_class_index", "AssetClass" },
+            {"currency_index", "Currency" },
+            {"value", "Value" },
+            {"fund_index", "Fund" }
+        };
+        private static AssetAllocationEntry defaultValue = new AssetAllocationEntry();
+        public AssetClass AssetClass { get; set; }
+        public Currency Currency { get; set; }
+        public Fund Fund { get; set; }
+        public double Value { get; set; }
+        public double Index { get; set; }
 
         public AssetAllocationEntry() { }
+
+        public string GetTableName()
+        {
+            return tableName;
+        }
+
+        public List<string> GetTableHeader()
+        {
+            return new List<string>(tableMapping.Keys);
+        }
+
+        public double GetIndex()
+        {
+            return Index;
+        }
+
+        public void SetIndex(double index)
+        {
+            Index = index;
+        }
+
+        public Dictionary<string, string> GetTableMapping()
+        {
+            return tableMapping;
+        }
+
+        public static Persistable GetDefaultValue()
+        {
+            return defaultValue;
+        }
     }
 }
--- Forms/EditAssetAllocation.cs
+++ Forms/EditAssetAllocation.cs
@@ -1,22 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 using Watchdog.Entities;
 using Watchdog.Persistence;
 
 namespace Watchdog.Forms
 {
+    // Plan zum Updaten der Tabelle. Wenn die Tabelle geladen wird, soll in die jeweilige Textbox gleich der zugehörige Wert geladen werden.
     public partial class EditAssetAllocation : Form
     {
+        private readonly Fund fund;
+        private bool updateable;
+        private double totalRow;
+        private double totalCol;
+
         public EditAssetAllocation(Fund fund = null)
         {
             InitializeComponent();
-            LoadTable();
             if (fund != null)
             {
+                this.fund = fund;
                 LabelFund.Text = fund.Name;
             }
+            LoadTable();
+            CalcTotals();
+            
         }
 
         private Label GenerateLabel<T>(string text, T bindingObject) where T : Persistable
@@ -36,10 +46,8 @@ namespace Watchdog.Forms
             };
             if (bindingObject != null)
             {
-                Binding binding = new Binding("", bindingObject, "");
+                Binding binding = new Binding(string.Empty, bindingObject, string.Empty);
                 label.DataBindings.Add(binding);
-                T obj = (T) label.DataBindings[0].DataSource;
-                MessageBox.Show(obj.ToString());
             }
             return label;
         }
@@ -79,6 +87,24 @@ namespace Watchdog.Forms
                 sum += number;
             }
             return sum;
+        }
+
+        private AssetAllocationEntry GetAssetAllocationEntry(AssetClass assetClass, Currency currency)
+        {
+            TableUtility tableUtility = new TableUtility(Globals.WatchdogAddIn.Application.ActiveWorkbook);
+            List<AssetAllocationEntry> entries = tableUtility.ConvertRangesToObjects<AssetAllocationEntry>(tableUtility.ReadAllRows(AssetAllocationEntry.GetDefaultValue().GetTableName()));
+            var entryQuery = from entry in entries
+                             where entry.AssetClass.Name.Equals(assetClass.Name)
+                             where entry.Currency.IsoCode.Equals(currency.IsoCode)
+                             where entry.Fund.Index == fund.Index
+                             select entry;
+
+            List<AssetAllocationEntry> list = entryQuery.ToList();
+            if (list.Count == 0)
+            {
+                return null;
+            }
+            return entryQuery.ToList()[0];
         }
 
         private void LoadTable()
@@ -123,6 +149,8 @@ namespace Watchdog.Forms
             {
                 for (int col = 1; col < numberOfColumns; col++)
                 {
+                    AssetClass assetClass = tableLayoutPanel1.GetControlFromPosition(col, 0).DataBindings[0].DataSource as AssetClass;
+                    Currency currency = tableLayoutPanel1.GetControlFromPosition(0, row).DataBindings[0].DataSource as Currency;
                     TextBox textBox = new TextBox
                     {
                         AutoSize = false,
@@ -132,18 +160,19 @@ namespace Watchdog.Forms
                         BorderStyle = BorderStyle.None,
                         TextAlign = HorizontalAlignment.Right
                     };
+
+                    AssetAllocationEntry entry = GetAssetAllocationEntry(assetClass, currency);
+                    if (entry != null)
+                    {
+                        updateable = true;
+                        textBox.Text = entry.Value.ToString();
+                        Binding binding = new Binding(string.Empty, entry, string.Empty);
+                        textBox.DataBindings.Add(binding);
+                    }
+
                     textBox.KeyUp += (tb, keyUp) =>
                     {
-                        TableLayoutPanelCellPosition position = tableLayoutPanel1.GetPositionFromControl(tb as TextBox);
-                        Label totalAssetClass = tableLayoutPanel1.GetControlFromPosition(position.Column, numberOfRows) as Label;
-                        Label totalCurrency = tableLayoutPanel1.GetControlFromPosition(numberOfColumns, position.Row) as Label;
-                        Label total = tableLayoutPanel1.GetControlFromPosition(numberOfColumns, numberOfRows) as Label;
-                        double rowSum = GetRowSum(position.Row);
-                        double columnSum = GetColumnSum(position.Column);
-                        totalCurrency.Text = rowSum.ToString();
-                        totalAssetClass.Text = columnSum.ToString();
-                        double totalSum = GetTotalSum();
-                        total.Text = totalSum.ToString();
+                        CalcTotals(tb);
                     };
                     textBox.KeyUp += (tb, keyUp) =>
                     {
@@ -171,6 +200,41 @@ namespace Watchdog.Forms
             }
         }
 
+        private void CalcTotals(object tb)
+        {
+            int numberOfRows = tableLayoutPanel1.RowCount - 1;
+            int numberOfColumns = tableLayoutPanel1.ColumnCount - 1;
+            TableLayoutPanelCellPosition position = tableLayoutPanel1.GetPositionFromControl(tb as TextBox);
+            Label totalAssetClass = tableLayoutPanel1.GetControlFromPosition(position.Column, numberOfRows) as Label;
+            Label totalCurrency = tableLayoutPanel1.GetControlFromPosition(numberOfColumns, position.Row) as Label;
+            Label total = tableLayoutPanel1.GetControlFromPosition(numberOfColumns, numberOfRows) as Label;
+            double rowSum = GetRowSum(position.Row);
+            double columnSum = GetColumnSum(position.Column);
+            totalCurrency.Text = rowSum.ToString();
+            totalAssetClass.Text = columnSum.ToString();
+            double totalSum = GetTotalSum();
+            total.Text = totalSum.ToString();
+        }
+
+        private void CalcTotals()
+        {
+            for (int col = 1; col < tableLayoutPanel1.ColumnCount - 1; col++)
+            {
+                double colSum = GetColumnSum(col);
+                totalCol += colSum;
+                Label label = tableLayoutPanel1.GetControlFromPosition(col, tableLayoutPanel1.RowCount - 1) as Label;
+                label.Text = colSum.ToString();
+            }
+            for (int row = 1; row < tableLayoutPanel1.RowCount - 1; row++)
+            {
+                double rowSum = GetRowSum(row);
+                totalRow += rowSum;
+                Label label = tableLayoutPanel1.GetControlFromPosition(tableLayoutPanel1.ColumnCount - 1, row) as Label;
+                label.Text = rowSum.ToString();
+            }
+            tableLayoutPanel1.GetControlFromPosition(tableLayoutPanel1.ColumnCount - 1, tableLayoutPanel1.RowCount - 1).Text = totalCol.ToString();
+        }
+
         private void CancelButtonClick(object sender, EventArgs e)
         {
             Close();
@@ -178,10 +242,43 @@ namespace Watchdog.Forms
 
         private void ButtonSubmit_Click(object sender, EventArgs e)
         {
-            for (int col = 1; col < tableLayoutPanel1.ColumnCount; col++)
+            TableUtility tableUtility = new TableUtility(Globals.WatchdogAddIn.Application.ActiveWorkbook);
+            tableUtility.CreateMissingTable(AssetAllocationEntry.GetDefaultValue());
+            CalcTotals();
+            for (int col = 1; col < tableLayoutPanel1.ColumnCount - 1; col++)
             {
-                // AssetClass colAssetClass = tableLayoutPanel1.GetControlFromPosition(col, 0);
+                for (int row = 1; row < tableLayoutPanel1.RowCount - 1; row++)
+                {
+                    bool parsingSuccessful = double.TryParse(tableLayoutPanel1.GetControlFromPosition(col, row).Text, out double value);
+                    if (!parsingSuccessful)
+                    {
+                        value = 0;
+                    }
+                    if (updateable)
+                    {
+                        AssetAllocationEntry entry = tableLayoutPanel1.GetControlFromPosition(col, row).DataBindings[0].DataSource as AssetAllocationEntry;
+                        TableUpdateWrapper update = new TableUpdateWrapper(entry.Index, "value", tableLayoutPanel1.GetControlFromPosition(col, row).Text);
+                        tableUtility.UpdateTableRow(entry, update);
+                    } else
+                    {
+                        AssetAllocationEntry assetAllocationEntry = new AssetAllocationEntry
+                        {
+                            AssetClass = tableLayoutPanel1.GetControlFromPosition(col, 0).DataBindings[0].DataSource as AssetClass,
+                            Currency = tableLayoutPanel1.GetControlFromPosition(0, row).DataBindings[0].DataSource as Currency,
+                            Value = value,
+                            Fund = fund
+                        };
+                        tableUtility.InsertTableRow(assetAllocationEntry, new List<string>
+                    {
+                        assetAllocationEntry.AssetClass.Index.ToString(),
+                        assetAllocationEntry.Currency.Index.ToString(),
+                        value.ToString(),
+                        fund.Index.ToString()
+                    });
+                    }
+                }
             }
+            Close();
         }
     }
 }
