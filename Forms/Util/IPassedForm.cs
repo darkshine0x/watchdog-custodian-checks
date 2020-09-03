41d80e95a7f70e9b247268d00d5863b5b6d062c1
jbaumann2 <jan.baumann2@outlook.com>
2020-09-03 22:56:56 +0200

    Set default parameters since they are not necessary everywhere

--- Forms/Util/IPassedForm.cs
+++ Forms/Util/IPassedForm.cs
@@ -4,6 +4,6 @@ namespace Watchdog.Forms.Util
 {
     public interface IPassedForm
     {
-        void OnSubmit(List<string> passedValue, string reference);
+        void OnSubmit(List<string> passedValue = null, string reference = null);
     }
 }
