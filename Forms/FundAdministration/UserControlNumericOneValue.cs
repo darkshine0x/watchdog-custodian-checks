bfb7f133f9eb5425ab5f42da66fb5123dda0005e
jbaumann2 <jan.baumann2@outlook.com>
2020-09-13 00:15:21 +0200

    Code documentation for UserControlNumericOneValue #30

--- Forms/FundAdministration/UserControlNumericOneValue.cs
+++ Forms/FundAdministration/UserControlNumericOneValue.cs
@@ -1,17 +1,34 @@
-﻿using System.Windows.Forms;
+﻿/*
+ * Author: Jan Baumann
+ * Version: 12.09.2020
+ */
+
+using System.Windows.Forms;
 using Watchdog.Entities;
 using Watchdog.Forms.Util;
 using Watchdog.Persistence;
 
 namespace Watchdog.Forms.FundAdministration
 {
+    /// <summary>
+    /// User Control for adding a new rules which only need a name and a numeric value-
+    /// </summary>
     public partial class UserControlNumericOneValue : UserControl, IRuleUserControl
     {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public UserControlNumericOneValue()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Implementation of <see cref="IRuleUserControl"/>. This method is called by the parent form 
+        /// for invoking the addition of a new rule.
+        /// </summary>
+        /// <param name="ruleKind"><see cref="RuleKind"/></param>
+        /// <returns>New <see cref="Rule"/></returns>
         public Rule InvokeSubmission(RuleKind ruleKind)
         {
             bool parsingSuccessful = double.TryParse(textBoxValue.Text, out double value);
