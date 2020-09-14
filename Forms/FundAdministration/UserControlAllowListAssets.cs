49cc531016771549752be0059eb729d92971f093
jbaumann2 <jan.baumann2@outlook.com>
2020-09-15 00:15:54 +0200

    Code documentation of UserControlAllowListAssets class #30

--- Forms/FundAdministration/UserControlAllowListAssets.cs
+++ Forms/FundAdministration/UserControlAllowListAssets.cs
@@ -1,16 +1,27 @@
-﻿using System.Windows.Forms;
+﻿/*
+ * Author: Jan Baumann
+ * Version: 15.09.2020
+ */
+
+using System.Windows.Forms;
 using Watchdog.Entities;
 using Watchdog.Forms.Util;
 using Watchdog.Persistence;
 
 namespace Watchdog.Forms.FundAdministration
 {
+    /// <summary>
+    /// Form for inserting or editing a rule for an <see cref="AllowList"/> with authorized assets.
+    /// </summary>
     public partial class UserControlAllowListAssets : UserControl, IPassedObject<Asset>, IRuleUserControl
     {
         private DataGridView dataGridView;
         private Button addNewAsset;
         private TextBox ruleName;
 
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public UserControlAllowListAssets()
         {
             InitializeComponent();
@@ -19,6 +30,9 @@ namespace Watchdog.Forms.FundAdministration
             tableUtility.CreateTable(Asset.GetDefaultValue());
         }
 
+        /// <summary>
+        /// Initialization of self-added controls
+        /// </summary>
         private void InitializeCustomComponents()
         {
             ruleName = FormUtility.CreateTextBox(20, 5);
@@ -42,6 +56,10 @@ namespace Watchdog.Forms.FundAdministration
             FormUtility.AddControlsToForm(this, dataGridView, addNewAsset, ruleName);
         }
 
+        /// <summary>
+        /// Implementation of <see cref="IPassedObject{T}"/>
+        /// </summary>
+        /// <param name="obj">From sub form passed object</param>
         public void OnSubmit(Asset obj)
         {
             TableUtility tableUtility = new TableUtility();
@@ -49,11 +67,20 @@ namespace Watchdog.Forms.FundAdministration
             FormUtility.GetBindingSource(dataGridView).Add(obj);
         }
 
+        /// <summary>
+        /// Gets the current selected <see cref="Asset"/>in the <see cref="DataGridView"/>
+        /// </summary>
+        /// <returns>Selected asset</returns>
         private Asset GetSelectedAsset()
         {
             return dataGridView.SelectedRows[0].DataBoundItem as Asset;
         }
 
+        /// <summary>
+        /// Implementation of <see cref="IRuleUserControl"/>
+        /// </summary>
+        /// <param name="ruleKind">Selected <see cref="RuleKind"/></param>
+        /// <returns>Newly created rule, in that case an <see cref="AllowList"/></returns>
         public Rule InvokeSubmission(RuleKind ruleKind)
         {
             AllowList newAllowList = new AllowList(RuleKind.RULE_EXCEPTION, ruleName.Text);
