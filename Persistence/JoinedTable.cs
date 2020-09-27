731bea117fb5103ac2837c77ca242b32e31e5fcf
jbaumann2 <jan.baumann2@outlook.com>
2020-09-27 11:25:09 +0200

    Added documentation #30

--- Persistence/JoinedTable.cs
+++ Persistence/JoinedTable.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿/*
+ * Author: Jan Baumann
+ * Version: 27.09.2020
+ */
+
+using System;
 
 namespace Watchdog.Persistence
 {
+    /// <summary>
+    /// Attribute for marking an entity as a joined table.
+    /// 
+    /// This attribute is used by the base class.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     class JoinedTable : Attribute
     {
+        /// <summary>
+        /// Name of the subclass.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Attribute constructor.
+        /// </summary>
+        /// <param name="name">Name of the base class</param>
         public JoinedTable(string name)
         {
             Name = name;
