0c174e732ba5a2fdd347b320dbe98e93632440a5
jbaumann2 <jan.baumann2@outlook.com>
2020-09-27 11:22:39 +0200

    Added documentation #30

--- Persistence/Persistable.cs
+++ Persistence/Persistable.cs
@@ -1,13 +1,38 @@
-﻿namespace Watchdog.Persistence
+﻿/*
+ * Author: Jan Baumann
+ * Version: 27.09.2020
+ */
+
+namespace Watchdog.Persistence
 {
+    /// <summary>
+    /// This interface requires the functionality for entities to be persistable.
+    /// </summary>
     public interface Persistable
     {
+        /// <summary>
+        /// Returns the name of the table in which the entities are stored.
+        /// </summary>
+        /// <returns>Table name</returns>
         string GetTableName();
 
+        /// <summary>
+        /// Gets the short name for subtables. This is important since the excel worksheets
+        /// have name length boundaries.
+        /// </summary>
+        /// <returns>Short name</returns>
         string GetShortName();
 
+        /// <summary>
+        /// Gets the primary key.
+        /// </summary>
+        /// <returns>Primary key</returns>
         double GetIndex();
 
+        /// <summary>
+        /// Sets the primary key.
+        /// </summary>
+        /// <param name="index">Primary key</param>
         void SetIndex(double index);
     }
 }
